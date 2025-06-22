64a798b8d83d036293f1c0f8b799f8ac23c5392d
ferid aqa <feridcodee@gmail.com>
2025-06-22 21:35:18 +0400

    ui edit

--- components/common/SafetyWarning.tsx
+++ components/common/SafetyWarning.tsx
@@ -7,7 +7,7 @@ export const SafetyWarning: React.FC = () => {
     <div className="bg-yellow-500 dark:bg-yellow-700 text-white dark:text-gray-100 p-3 text-center text-sm font-medium flex items-center justify-center gap-2">
       <ExclamationTriangleIcon  className="h-5 w-5 text-yellow-800 dark:text-yellow-200" />
       <span>
-        Təhlükəsizlik tədbirlərinə görə saytda məxfi məlumatlarınızı daxil etməyinizi tələb olunur.
+        Təhlükəsizlik tədbirlərinə görə saytda məxfi məlumatlarınızı daxil "etməməyiniz" tələb olunur.
       </span>
     </div>
   );
